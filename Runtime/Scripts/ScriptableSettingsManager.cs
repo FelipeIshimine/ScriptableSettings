8f9dda404a0b2fc385a898d6b3c89c07db61e447
Felipe Ishimine <felipeishimine@gmail.com>
2020-10-21 11:31:43 -0300

    a

--- Runtime/Scripts/ScriptableSettingsManager.cs
+++ Runtime/Scripts/ScriptableSettingsManager.cs
@@ -14,11 +14,8 @@ public class ScriptableSettingsManager : RuntimeScriptableSingleton<ScriptableSe
 #if UNITY_EDITOR
     public static class UpdateGit
     {
-        [MenuItem("Window/Ishimine/UpdateScriptableSettings")]
-        public static void SelectMe()
-        {
-            AddRequest request = Client.Add("https://github.com/FelipeIshimine/ScriptableSettings.git");
-        }
+        [MenuItem("Window/Ishimine/Update/ScriptableSettings")]
+        public static void SelectMe() => Client.Add("https://github.com/FelipeIshimine/ScriptableSettings.git");
     }
 #endif
 
